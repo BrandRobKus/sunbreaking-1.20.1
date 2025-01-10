2f7ebeda3f7384ea060bffe6d7f8a8ae2d486a8c
BrandRobKus <brandrobkus@gmail.com>
2025-01-10 13:19:28 -0500

    Initialization of mod

---
+++ src/main/java/com/brandrobkus/sunbreaking/SunbreakingClient.java
@@ -0,0 +1,10 @@
+package com.brandrobkus.sunbreaking;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SunbreakingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
